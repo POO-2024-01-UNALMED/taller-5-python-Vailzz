d9237c61623e57f39a01a73a596d9b87b8e6fae6
Juan Villa <jujotovil@gmail.com>
2024-03-18 23:24:08 -0500

    Commit

--- gestion/Zona.py
+++ gestion/Zona.py
@@ -1,11 +1,33 @@
+from typing import Any
+from zooAnimales.animal import Animal
+from gestion.zoologico import Zoologico
+
 class Zona:
-    def __init__(self, nombre, zoo, animales):
-        self.nombre = nombre
-        self.zoo = zoo
-        self.animales = animales
     
-    def agregarAnimales(self, animal):
-        self.animales.append(animal)
+  def __init__(self, nombre : str | None = None,zoologico : Zoologico | None = None,animales : list | None = None):
+    self._nombre = nombre
+    self._animales = animales if animales is not None else []
+    self._zoologico = zoologico
     
-    def cantidadAnimales(self):
-        return len(self.animales)
+  def agregarAnimales(self, animal : Animal):
+    self._animales.append(animal)
+    
+  def cantidadAnimales(self):
+    return len(self._animales)
+    
+  def getNombre(self) -> str | None:
+    return self._nombre
+    
+  def setNombre(self, nombre : str) -> None:
+    self._nombre = nombre
+    
+  def getAnimales(self) -> list:
+    return self._animales
+    
+  def setAnimales(self, animales : list) -> None:
+    self._animales = animales
+    
+  def getZoo(self):
+    return self._zoologico
+  
+  pass
--- gestion/Zoologico.py
+++ gestion/Zoologico.py
@@ -1,14 +1,40 @@
+
+from zooAnimales.animal import Animal
+
 class Zoologico:
-    def __init__(self, nombre, ubicacion, zonas):
-        self.nombre = nombre
-        self.ubicacion = ubicacion
-        self.zonas = zonas
-    
-    def agregarZonas(self, zona):
-        self.zonas.append(zona)
-    
-    def cantidadTotalAnimales(self):
-        cantidad = 0
-        for zona in self.zonas:
-            cantidad += zona.cantidadAnimales()
-        return cantidad
+
+  def __init__(self,
+               nombre: str | None = None,
+               ubicacion: str | None = None,
+               zonas: list = []):
+    self._nombre = nombre
+    self._ubicacion = ubicacion
+    self._zonas = zonas
+
+  def agregarZonas(self, zona):
+    if zona != None:
+      self._zonas.append(zona)
+
+  def cantidadTotalAnimales(self):
+    return sum(z.cantidadAnimales() for z in self.getZona())
+
+  def getNombre(self) -> str | None:
+    return self._nombre
+
+  def setNombre(self, nombre: str) -> None:
+    self._nombre = nombre
+
+  def getUbicacion(self) -> str | None:
+    return self._ubicacion
+
+  def setUbicacion(self, ubicacion: str) -> None:
+    self._ubicacion = ubicacion
+
+  def getZona(self) -> list:
+    return self._zonas
+
+  def setZona(self, zonas: list) -> None:
+    self._zonas = zonas
+
+  pass
+
